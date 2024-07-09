d204332495cb037ea8ce12f4dd05e81e4f049e6a
bidis <nikcharg4@gmail.com>
2024-07-09 10:10:29 +0300

    Add comments

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,6 +10,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
 
+  // Handle validation error
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((item) => item.message)
